690b76ea06940c850c57bbfba7588e7c4b24d4e8
rocio <contactolda@gmail.com>
2020-03-13 02:10:09 -0300

    desfiocomponentes2

---
+++ src/App.vue
@@ -0,0 +1,91 @@
+<template>
+    <div id="main">
+            <span class="titulo">{{ message }}</span>
+        <br>
+        <br>
+            <label>Ingrese Tarea: </label>
+            <input type="text" v-model="tarea"/>
+
+            <button @click="guardarTarea">Ingresar tarea</button>
+            <p v-if="mostrar">{{nombre}}</p>
+        <br>
+        <br>
+            <label>Lista de Tareas:</label>
+        <lista-tareas @receptorIndexBorrarTarea="borrarTarea" :tareas="tareas" > </lista-tareas>
+          
+    </div>
+</template>
+
+<script>
+import ListaTareas from "./components/ListaTareas.vue"
+    export default {
+        data(){
+            return{
+                message:"Agenda de Tareas",
+                tarea:"",
+                tareas:[],
+                mostrar:false
+            }
+        },
+        methods: {
+            mostrarTarea: function(){
+                this.mostrar = !this.mostrar;
+            },
+            guardarTarea: function(){
+                if(this.tarea !== ""){
+                    this.tareas.push(
+                        this.tarea
+                    );
+                }
+                this.tarea="";
+            },
+            borrarTarea: function(index){
+                this.tareas.splice(index, 1);
+                
+            }
+
+        },
+        components:{
+            ListaTareas
+        }      
+    };
+</script>
+<style scoped>
+
+    * {
+    margin: 0 auto;
+    border: 0;
+    padding: 0;
+    font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif
+    }
+    #main {
+        margin-top: 40px;
+        text-align: center;
+        font-size: 25px;
+        font-family: 'Calibri', sans-serif;
+        background: rgb(85, 255, 210);
+        color: rgb(91, 91, 91);
+        padding: 5em;
+        width: 40rem;
+        height: 20rem; 
+    }
+
+   
+
+    input {
+        width: 180px;
+        height: 25px;
+        border-radius: 10px;
+    }
+    button {
+        width: 100px;
+        height: 25px;
+        border-radius: 10px;
+
+    }
+    .titulo {
+        font-size: 30px;
+    }
+
+
+</style>
---
+++ src/components/ListaTareas.vue
@@ -0,0 +1,46 @@
+<template>
+    
+    <div class="container"> 
+    <ul v-for="(tarea, index) in tareas" :key="index" >
+        <li>{{tarea}}<button @click="emitirMensajeDeBorrar" v-bind:value="index">Eliminar</button></li>
+    </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ListaTareas",
+    props: {
+        tareas:Array
+    },
+    methods:{
+        emitirMensajeDeBorrar: function(event){
+            let indiceBorrarTarea = event.target.value;
+            event.target.checked = false;
+            this.$emit("receptorIndexBorrarTarea",indiceBorrarTarea);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container  {
+  width: 30rem;
+  margin: 0 auto;
+  text-align: left;
+  margin-left: 33%;
+}
+
+button {
+        width: 80px;
+        height: 20px;
+        border-radius: 10px;
+        margin-left: 1rem;
+        border: none;
+    }
+
+li {
+
+    font-size: 20px;
+}
+</style>
